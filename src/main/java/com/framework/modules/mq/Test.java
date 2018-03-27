18ea217a9f6bda3cfcaf91ddc3bd533caf93ca58
yuan <yuan@DESKTOP-8UVDU0U>
2018-03-27 09:55:32 +0800

    jdk1.8

--- src/main/java/com/framework/modules/mq/Test.java
+++ src/main/java/com/framework/modules/mq/Test.java
@@ -1,13 +1,11 @@
 package com.framework.modules.mq;
 
 public class Test {
-	
-	public static void test() {
-		
-	}
-	
-	public class Test2{
-		
+
+	public static void main(String[] a) {
+		new Thread(() -> {
+			System.out.println(2);
+		}).start();
 	}
 
 }
